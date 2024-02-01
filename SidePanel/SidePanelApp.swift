5bf72c96d95b1af381f5e8fff758b851d73ab205
Yan Moroz <mail@yanmoroz.ru>
2024-02-01 22:40:04 +0300

    Initial Commit

---
+++ SidePanel/SidePanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidePanelApp.swift
+//  SidePanel
+//
+//  Created by Yan Moroz on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SidePanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
